6e0438375ff9839f1eeac803fad8ffb0ebf1c790
HuynhNg <huynhbbb963@gmail.com>
2024-05-20 20:49:40 +0700

    Page

--- Page.js
+++ Page.js
@@ -5,13 +5,10 @@ function Mydetail() {
     const detail = document.querySelector('.detail');
     detail.style.display = "flex";
 }
-document.addEventListener('click', function(event) {
-    const detailContainer = document.getElementById('detail_container');
-    const detail = document.getElementById('detail');
-    if (!detailContainer.contains(event.target) && !event.target.closest('button')) {
-        detail.classList.add('hidden');
-    }
-});
+
 function Close(){
     document.getElementById("Form").style.display= "none";
+}
+function MyCloseDetail(){
+    document.getElementById("detail").style.display= "none";
 }
