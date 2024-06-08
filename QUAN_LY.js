bcb255645689de5a5c6dd33710c715adc2954ed0
HuynhNg <huynhbbb963@gmail.com>
2024-06-09 03:37:54 +0700

    Update

--- QUAN_LY.js
+++ QUAN_LY.js
@@ -82,3 +82,40 @@ function CloseForm(ID){
 function TrangChu(){
     window.location.href="./Page.html";
 }
+function Update(element) {
+    // Get the parent row of the clicked element
+    var row = element.parentElement.parentElement;
+    let Titles = row.parentElement.querySelector('tr').querySelectorAll('th');
+    let Container = document.getElementById("Update_container");
+    console.log(element.closest('.manager_container').id);
+    // Clear the container content before updating
+    Container.innerHTML = '';
+
+    // Add title for the form
+    Container.innerHTML += `
+        <div class="Form_item" style="justify-content: center;">
+            <p style="font-weight: 600;font-size: 24px;color: black;">Thông tin</p>
+        </div>
+    `;
+    // Iterate through each title and corresponding cell in the row
+    Titles.forEach(function(title, index) {
+        if(index != Titles.length-1 && index != 0)
+        {
+            Container.innerHTML += `
+            <div class="Form_item">
+                <p>${title.innerText}</p>
+                <input type="text" class="input" value="${row.cells[index].innerText}">
+            </div>
+        `;
+        }
+    });
+    Container.innerHTML+= `
+        <div class="Form_item" style="justify-content: center;margin-top: 20px;">
+            <button class="button1">Chỉnh sửa</button>
+            <button class="button1" onclick="CloseForm('Update')">Thoát</button>
+        </div>
+    `;
+
+    // Correcting the display property
+    document.getElementById("Update").style.display = "flex";
+}
